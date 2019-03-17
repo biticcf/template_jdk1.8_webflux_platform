3c76e1f97e4e620abefb86451d6ffdf97233ceb7
CaoChaofeng <caochaofeng@wanda.cn>
2019-03-17 20:36:02 +0800

    配置Mybatis和PageHelper支持多数据源

--- template/@__scope__@-@__template__@-platform.git/@__scope__@-@__template__@-server/src/main/java/com/@__company__@/@__scope__@/@__template__@/app/Main.java
+++ template/@__scope__@-@__template__@-platform.git/@__scope__@-@__template__@-server/src/main/java/com/@__company__@/@__scope__@/@__template__@/app/Main.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import com.beyonds.phoenix.mountain.shackle.EnableShackleTemplates;
+import com.github.pagehelper.autoconfigure.PageHelperAutoConfiguration;
 
 /**
  * @Author: DanielCao
@@ -21,7 +22,7 @@ import com.beyonds.phoenix.mountain.shackle.EnableShackleTemplates;
  *     @Configuration+@EnableAutoConfiguration+@ComponentScan
  *
  */
-@SpringBootApplication(scanBasePackages = {"com.@__company__@.@__scope__@.@__template__@"})
+@SpringBootApplication(scanBasePackages = {"com.@__company__@.@__scope__@.@__template__@"}, exclude = {PageHelperAutoConfiguration.class})
 @EnableFeignClients(basePackages = {"com.@__company__@.@__scope__@.@__template__@.domain.feign"})
 @EnableShackleTemplates(basePackages = {"com.@__company__@.@__scope__@.@__template__@.service"})
 @EnableEurekaClient
