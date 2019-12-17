d8e0fbd154ef52ac4027e8a4174143469593a71e
CaoChaofeng <caochaofeng@wanda.cn>
2019-12-17 23:39:58 +0800

    增加带有access log日志过滤功能的tomcat容器；核心依赖包版本升级到5.2.RELEASE。

--- template/@__scope__@-@__template__@-platform.git/@__scope__@-@__template__@-server/src/main/java/com/@__company__@/@__scope__@/@__template__@/web/config/WebFluxConfiguration.java
+++ template/@__scope__@-@__template__@-platform.git/@__scope__@-@__template__@-server/src/main/java/com/@__company__@/@__scope__@/@__template__@/web/config/WebFluxConfiguration.java
@@ -6,8 +6,11 @@ package com.@__company__@.@__scope__@.@__template__@.web.config;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.UpgradeProtocol;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.http.HttpProperties;
 import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
@@ -22,7 +25,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -30,6 +35,8 @@ import org.springframework.web.reactive.result.view.ViewResolver;
 
 import com.github.biticcf.mountain.core.common.service.StringDateConverter;
 import com.github.biticcf.mountain.core.common.service.StringDecoderForHeaderConverter;
+import com.github.biticcf.mountain.core.common.tomcat.FiltedAccessLogProperties;
+import com.github.biticcf.mountain.core.common.tomcat.FiltedTomcatWebServerFactoryCustomizer;
 import com.@__company__@.@__scope__@.@__template__@.web.controller.DefaultErrorHandler;
 
 /**
@@ -39,7 +46,7 @@ import com.@__company__@.@__scope__@.@__template__@.web.controller.DefaultErrorH
  *
  */
 @Configuration(proxyBeanMethods = false)
-@EnableConfigurationProperties({ HttpProperties.class })
+@EnableConfigurationProperties({ HttpProperties.class, ServerProperties.class, FiltedAccessLogProperties.class })
 @Import({WebFluxAutoConfiguration.class})
 @ComponentScan(
         value = "com.@__company__@.@__scope__@.@__template__@.web",
@@ -91,5 +98,20 @@ public class WebFluxConfiguration implements WebFluxConfigurer {
     @Bean
     public StringDecoderForHeaderConverter stringHeaderConverter(HttpProperties httpProperties) {
         return new StringDecoderForHeaderConverter(httpProperties.getEncoding().getCharset());
+    }
+    
+    /**
+     * +自定义带有access log过滤功能的tomcat容器
+     * @param environment environment
+     * @param serverProperties serverProperties
+     * @param filtedAccessLogProperties filtedAccessLogProperties
+     * @return FiltedTomcatWebServerFactoryCustomizer
+     */
+    @Primary
+    @ConditionalOnClass({ Tomcat.class, UpgradeProtocol.class })
+    @Bean
+    public FiltedTomcatWebServerFactoryCustomizer filtedTomcatWebServerFactoryCustomizer(Environment environment,
+                ServerProperties serverProperties, FiltedAccessLogProperties filtedAccessLogProperties) {
+        return new FiltedTomcatWebServerFactoryCustomizer(environment, serverProperties, filtedAccessLogProperties);
     }
 }
